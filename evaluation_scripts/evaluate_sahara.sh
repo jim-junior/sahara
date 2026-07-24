e2e8d0302955f50df6564d24fb40df9a9249a556
jim-junior <jimjunior854@gmail.com>
2026-07-24 10:34:11 +0300

    ft: configure evaluate script for sunflower vasta instance

--- evaluation_scripts/evaluate_sahara.sh
+++ evaluation_scripts/evaluate_sahara.sh
@@ -1,10 +1,7 @@
 
-
-huggingface-cli login --token your_tokens
-
-model_id=Qwen/Qwen3-4B-Instruct-2507
+model_id=Sunbird/Sunflower-Qwen3.5-9B
 provider=vllm
-cache_dir=/path/to/cache/dir
+cache_dir=/workspace/.cache
 
 CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES="0,1,2,4" \
             python run_sahara_eval_colab_v1.py --provider $provider --model_id $model_id --tasks \
