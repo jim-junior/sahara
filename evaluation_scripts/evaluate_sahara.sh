ede6d9787af69d303e4140bb4273019388d92a68
jim-junior <jimjunior854@gmail.com>
2026-07-29 12:44:07 +0300

    ft: expand tasks in evaluate_sahara.sh for comprehensive evaluation

--- evaluation_scripts/evaluate_sahara.sh
+++ evaluation_scripts/evaluate_sahara.sh
@@ -5,5 +5,9 @@ cache_dir=/workspace/.cache
 
 CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES="0,1,2,4" \
             python run_sahara_eval_colab_v1.py --provider $provider --model_id $model_id --tasks \
-                                        sentiment \
+                                        news title summary \
+                                        sentiment topic xlni lid \
+                                        paraphrase mt_eng2xx mt_fra2xx mt_xx2xx \
+                                        mmlu mgsm belebele squad_qa \
+                                        phrase pos ner \
                                         --batch_size 1000 --cache_dir $cache_dir
